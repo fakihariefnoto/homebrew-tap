f43eb3164194c4f34d865d8c4d24a7aac21818c1
fakihariefnoto <fakihariefnoto@gmail.com>
2026-08-13 02:41:36 +0700

    Rebuild v0.1.0 archives: plain-prompt wizard, no Bubble Tea (fixes blank UI)

--- Formula/commitly.rb
+++ Formula/commitly.rb
@@ -2,17 +2,17 @@ class Commitly < Formula
   desc "Compose Conventional Commits messages, interactively"
   homepage "https://github.com/fakihariefnoto/commitly"
   url "https://github.com/fakihariefnoto/commitly/releases/download/v0.1.0/commitly_0.1.0_darwin_arm64.tar.gz"
-  sha256 "062f8263f057e929b9a33308f3e3ed2cc7de78e06a6a4d13f36dbfe9749430b1"
+  sha256 "30dc45ef21d75a63ea7c9e3c4cfe39d4019b1c114bf5780f3c6012307d9f057a"
   license "MIT"
 
   on_arm do
     url "https://github.com/fakihariefnoto/commitly/releases/download/v0.1.0/commitly_0.1.0_darwin_arm64.tar.gz"
-    sha256 "062f8263f057e929b9a33308f3e3ed2cc7de78e06a6a4d13f36dbfe9749430b1"
+    sha256 "30dc45ef21d75a63ea7c9e3c4cfe39d4019b1c114bf5780f3c6012307d9f057a"
   end
 
   on_intel do
     url "https://github.com/fakihariefnoto/commitly/releases/download/v0.1.0/commitly_0.1.0_darwin_amd64.tar.gz"
-    sha256 "fcec7f9565fd90c3893d167248cee751fbe888ca440fc3f6552df719ea543161"
+    sha256 "c58c6e199e664cb04955fabe453f5e0945cb14759503bd104d9ba7958b1f2186"
   end
 
   def install
