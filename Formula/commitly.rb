d84ff889ba3cd511a6c6867d325e6440a6c81c86
fakihariefnoto <fakihariefnoto@gmail.com>
2026-08-13 10:03:07 +0700

    Rebuild v0.1.0 archives: proper Bubble Tea list UI

--- Formula/commitly.rb
+++ Formula/commitly.rb
@@ -2,17 +2,17 @@ class Commitly < Formula
   desc "Compose Conventional Commits messages, interactively"
   homepage "https://github.com/fakihariefnoto/commitly"
   url "https://github.com/fakihariefnoto/commitly/releases/download/v0.1.0/commitly_0.1.0_darwin_arm64.tar.gz"
-  sha256 "98778e711bf53647d83364f77233be29cfb9864ba2ba7e370330ce671382e0b5"
+  sha256 "c01981f86638e5eafd48cad0448c5f86d3083a6af1c2521bcb5e0c0ab354fa23"
   license "MIT"
 
   on_arm do
     url "https://github.com/fakihariefnoto/commitly/releases/download/v0.1.0/commitly_0.1.0_darwin_arm64.tar.gz"
-    sha256 "98778e711bf53647d83364f77233be29cfb9864ba2ba7e370330ce671382e0b5"
+    sha256 "c01981f86638e5eafd48cad0448c5f86d3083a6af1c2521bcb5e0c0ab354fa23"
   end
 
   on_intel do
     url "https://github.com/fakihariefnoto/commitly/releases/download/v0.1.0/commitly_0.1.0_darwin_amd64.tar.gz"
-    sha256 "e20e318e6302669b6059b6e5d24da44040a75695880b88e52470f43f59e8f415"
+    sha256 "d7de4e77fc1d6b7b6f885f0071932c0b9d6917909352cd3e04f86ccc4451ecdd"
   end
 
   def install
